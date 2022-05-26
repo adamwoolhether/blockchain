ac84c933998351cbfd792dac64dbafbd885bdb50
adamwoolhether <adamwoolhether@gmail.com>
2022-05-26 13:11:57 +0800

    charge gas on bad transactions

--- foundation/blockchain/state/transaction.go
+++ foundation/blockchain/state/transaction.go
@@ -6,7 +6,11 @@ import (
 
 // UpsertWalletTransaction accepts a transaction from a wallet for inclusion.
 func (s *State) UpsertWalletTransaction(signedTx database.SignedTx) error {
-	if err := s.validateTransaction(signedTx); err != nil {
+
+	// Check the signed transaction has proper signature and valid
+	// account for the recipient. The wallet should ensure the
+	// account has a proper balance. Fees are taken if tx is mined into block.
+	if err := signedTx.Validate(); err != nil {
 		return err
 	}
 
@@ -24,7 +28,10 @@ func (s *State) UpsertWalletTransaction(signedTx database.SignedTx) error {
 
 // UpsertNodeTransaction accepts a transaction from a node for inclusion.
 func (s *State) UpsertNodeTransaction(tx database.BlockTx) error {
-	if err := s.validateTransaction(tx.SignedTx); err != nil {
+
+	// Check the signed transaction has a proper signature and
+	// valid account for the signature.
+	if err := tx.Validate(); err != nil {
 		return err
 	}
 
